ad12f07058b5c564dc356a96940dee079feeae6d
benpapac <ben.papac@gmail.com>
2022-10-29 13:31:21 -0700

    add first draft of reducer and interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,19 +1,27 @@
  
-interface Ship {
-    owner: Player,
-    coordinates: Array<number>,
-    color: string,
+export interface shipInterface {
+    owner: playerInterface;
+    coordinates: Array<number>;
+    color: string;
 
  }
 
- interface Shot {
-    coordinates: Array<number>,
-    hp: number,
+ export interface shotInterface {
+    id: number;
+    coordinates: Array<number>;
+    hp: number;
 
  }
 
 
- interface Player {
-    ships: Array<Ship>,
-    color: string,
+ export interface playerInterface {
+    ships: Array<shipInterface>;
+    color: string;
+ }
+
+
+ export interface actionInterface {
+    type: string;
+    item: string;
+    value: Array<any>;
  }
